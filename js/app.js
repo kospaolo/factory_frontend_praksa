cfb1eba0b9052b50b16075d4a054c65a87769a1e
Paolo Kos <kos.paolo@gmail.com>
2021-09-29 14:30:05 +0200

    almost done

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 $(document).ready(() => {
 
- let upperSlider = document.getElementsByClassName('upperSlider')
- let bottomSlider = document.getElementsByClassName('bottomSlider')
- let thumbnails = document.getElementsByClassName('bottomSliderThumbnailsContainer')
+ let upperSlider = document.getElementsByClassName('upper-slider')
+ let bottomSlider = document.getElementsByClassName('bottom-slider')
+ let thumbnails = document.getElementsByClassName('bottom-slider-thumbnails-container')
+ let editorials = document.getElementsByClassName('editorials-content-container')
+ let editorialsPrev = document.getElementsByClassName('editorials-prev')
+ let editorialsNext = document.getElementsByClassName('editorials-next')
+ let localNews = document.getElementsByClassName('local-news-content-container')
+ let localNewsPrev = document.getElementsByClassName('local-news-prev')
+ let localNewsNext = document.getElementsByClassName('local-news-next')
+ let newsCarousel = document.getElementsByClassName('news-carousel-content-container')
+ let newsCarouselPrev = document.getElementsByClassName('news-carousel-prev')
+ let newsCarouselNext = document.getElementsByClassName('news-carousel-next')
 
  $(upperSlider).slick({
   prevArrow: "<span class='prev'><img class='a-left control-c prev slick-prev' src='./assets/sliderleftarrow.png'></span>",
-  nextArrow: "<span class='next'><img class='a-right control-c next slick-next' src='./assets/sliderrightarrow.png'></span>"
+  nextArrow: "<span class='next'><img class='a-right control-c next slick-next' src='./assets/sliderrightarrow.png'></span>",
+  autoplay: true
  });
 
  $(bottomSlider).slick({
@@ -14,17 +24,63 @@ $(document).ready(() => {
   nextArrow: "<span class='next'><img class='a-right control-c next slick-next' src='./assets/sliderrightarrow.png'></span>",
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: true,
-  fade: true,
-  asNavFor: '.bottomSliderThumbnailsContainer'
+  arrows: true
  });
 
  $(thumbnails).slick({
-  slidesToShow: 6,
+  slidesToShow: 7,
   slidesToScroll: 1,
-  arrows: false,
-  asNavFor: '.bottomSlider',
+  asNavFor: bottomSlider,
   centerMode: false,
   focusOnSelect: true
  });
+
+ $(newsCarousel).slick({
+  prevArrow: newsCarouselPrev,
+  nextArrow: newsCarouselNext,
+  slidesToShow: 2,
+  slidesToScroll: 2
+ });
+
+ $(editorials).slick({
+  prevArrow: editorialsPrev,
+  nextArrow: editorialsNext
+ });
+
+ $(localNews).slick({
+  prevArrow: localNewsPrev,
+  nextArrow: localNewsNext
+ });
+
+
+ $('img[data-enlargeable]').addClass('img-enlargeable').click(function () {
+
+  let src = $(this).attr('src');
+  let modal;
+
+  function removeModal() {
+   modal.remove();
+   $('body').off('keyup.modal-close');
+  }
+  modal = $('<div>').css({
+   background: 'RGBA(0,0,0,.5) url(' + src + ') no-repeat center',
+   backgroundSize: 'contain',
+   width: '100%',
+   height: '100%',
+   position: 'fixed',
+   zIndex: '10000',
+   top: '0',
+   left: '0',
+   cursor: 'pointer'
+
+  }).click(function () {
+   removeModal();
+  }).appendTo('body');
+  $('body').on('keyup.modal-close', function (e) {
+   if (e.key === 'Escape') {
+    removeModal();
+   }
+  });
+ });
+
 });
